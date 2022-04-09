4512cb6c64aa846b6eda578dfac10cbd8005ce0e
VladBaranchuk <54992069+VladBaranchuk@users.noreply.github.com>
2022-04-09 23:40:38 +0300

    add support DI for data access layer

---
+++ MyArt/MyArt.API/Infrastructure/Configurations/DependenciesConfiguration.cs
@@ -0,0 +1,49 @@
+﻿using MyArt.DataAccess.Contracts;
+using MyArt.DataAccess.Contracts.Providers;
+using MyArt.DataAccess.Contracts.Repositories;
+using MyArt.DataAccess.Providers;
+using MyArt.DataAccess.Repositories;
+using MyArt.Domain.Entities;
+
+namespace MyArt.API.Infrastructure.Configurations
+{
+    public static class DependenciesConfiguration
+    {
+        public static IServiceCollection AddDataAccess(this IServiceCollection services)
+        {
+            // IRepository
+            services.AddScoped<IRepository<ArtForm>, ArtFormRepository>();
+            services.AddScoped<IArtFormRepository, ArtFormRepository>();
+            services.AddScoped<IRepository<Art>, ArtRepository>();
+            services.AddScoped<IArtRepository, ArtRepository>();
+            services.AddScoped<IRepository<Board>, BoardRepository>();
+            services.AddScoped<IBoardRepository, BoardRepository>();
+            services.AddScoped<IRepository<Exhibition>, ExhibitionRepository>();
+            services.AddScoped<IExhibitionRepository, ExhibitionRepository>();
+            services.AddScoped<IRepository<Film>, FilmRepository>();
+            services.AddScoped<IFilmRepository, FilmRepository>();
+            services.AddScoped<IRepository<Genre>, GenreRepository>();
+            services.AddScoped<IGenreRepository, GenreRepository>();
+            services.AddScoped<IRepository<User>, UserRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            // IProvider
+            services.AddScoped<IProvider<ArtForm>, ArtFormProvider>();
+            services.AddScoped<IArtFormProvider, ArtFormProvider>();
+            services.AddScoped<IProvider<Art>, ArtProvider>();
+            services.AddScoped<IArtProvider, ArtProvider>();
+            services.AddScoped<IProvider<Board>, BoardProvider>();
+            services.AddScoped<IBoardProvider, BoardProvider>();
+            services.AddScoped<IProvider<Exhibition>, ExhibitionProvider>();
+            services.AddScoped<IExhibitionProvider, ExhibitionProvider>();
+            services.AddScoped<IProvider<Film>, FilmProvider>();
+            services.AddScoped<IFilmProvider, FilmProvider>();
+            services.AddScoped<IProvider<Genre>, GenreProvider>();
+            services.AddScoped<IGenreProvider, GenreProvider>();
+            services.AddScoped<IProvider<User>, UserProvider>();
+            services.AddScoped<IUserProvider, UserProvider>();
+
+            return services;
+        }
+    }
+}
--- MyArt/MyArt.API/Program.cs
+++ MyArt/MyArt.API/Program.cs
@@ -1,19 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using MyArt.API.Infrastructure.Configurations;
+using MyArt.DataAccess;
+using MyArt.DataAccess.Contracts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlServer("Server=.\\SQLEXPRESS;Database=MyArt;Trusted_Connection=true"));
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<IDataContext, DataContext>();
+
+builder.Services.AddDataAccess();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+
 }
 
 app.UseHttpsRedirection();
